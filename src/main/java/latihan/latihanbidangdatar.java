2be8a30aca8b05421d15463f98c51a5fcbbd2a95
USER <USER@3E-08>
2026-09-15 09:07:51 +0700

    TUGAS SAYA

---
+++ src/main/java/latihan/latihanbidangdatar.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan;
+public class latihanbidangdatar {
+    public static void main(String[] args) {
+        double panjang = 10;
+        double lebar = 5;
+
+        double luas = panjang * lebar;
+        double keliling = 2 * (panjang + lebar);
+
+        System.out.println("=== Menghitung Persegi Panjang ===");
+        System.out.println("Panjang  : " + panjang);
+        System.out.println("Lebar    : " + lebar);
+        System.out.println("Luas     : " + luas);
+        System.out.println("Keliling : " + keliling);
+    }
+}
